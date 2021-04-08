71d7eb36fd0ef5f58f0af53a51084b4f46e82fad
BlueSum <bluesum@protonmail.com>
2021-04-08 15:26:21 +0000

    stringss changedd

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'640004235c58a9d5dc58f3415fbc376b04d281f23fd76901f03ed742ff854c74' // init code hash
             ))));
     }
 
